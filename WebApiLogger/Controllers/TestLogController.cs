5db027810012a682635e7af947600caf07bad545
TI0301 <wayne_shen@tpe.emo-5.com.tw>
2020-10-20 15:57:17 +0800

    git init

---
+++ WebApiLogger/Controllers/TestLogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApiLogger.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TestLogController : ApiController
+    {
+        [HttpGet]
+        public object TestGet()
+        {
+            return new { status = "200", msg = "test ok" };
+        }
+    }
+}
